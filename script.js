9acc14859d80fd3d53d1581c266a84873216f668
efogerson1 <efogerson2@gmail.com>
2023-06-12 11:35:08 -0700

    recieving data from 5 day forecast call

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ var button = document.querySelector('.button');
 var userInput = document.querySelector('.userInput');
 var cityName = document.querySelector('.name');
 var temp = document.querySelector('.temp');
-var desc = document.querySelector('.desc');
 var humidity = document.querySelector('.humidity');
 var wind = document.querySelector('.wind');
+var fiveDayTemp = document.querySelector('.forecastTemp')
+var fiveDayHumidity = document.querySelector('.forecastHumidity')
+var fiveDayWind = document.querySelector('.forecastWind')
 
 
 /*  Test function with console.log */
@@ -27,7 +29,7 @@ function getCityLocation(){
            singleDay(lat, lon);
            fiveDayForecast(lat, lon);
          })
-         .catch(error => alert("Wrong!"))
+         .catch(error => alert(error));
      
        
 
@@ -41,15 +43,14 @@ function singleDay(lat, lon){
          .then(data => {
          
            var cityNameValue = data['name'];
-
            var tempValue = data['main']['temp'];
            var windValue = data['wind']['speed'];
            var humidityValue = data['main']['humidity'];
 
-          cityName.innerHTML = cityNameValue; 
-          temp.innerHTML = tempValue;
-          wind.innerHTML = windValue;
-          humidity.innerHTML = humidityValue;
+          cityName.innerHTML = 'City: '+ cityNameValue; 
+          temp.innerHTML = 'Temperature: '+ tempValue;
+          wind.innerHTML = 'Wind: '+ windValue;
+          humidity.innerHTML = 'Humidity: '+ humidityValue;
 
             
           /*  console.log(data); */
@@ -60,37 +61,30 @@ function singleDay(lat, lon){
 }
 
 function fiveDayForecast(lat, lon){
-  var fiveForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=e17d6c40bd10dd5e7fb0d651afd5e253`
+  var fiveForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=e17d6c40bd10dd5e7fb0d651afd5e253`
 
   fetch(fiveForecast)
          .then(response => response.json())
          .then(data => {
           for (let i = 0; i < data.list.length; i += 8){
             console.log(data.list[i]);
+
           }
          }); 
 
-      
+       var fiveDayTempValue = data[0]['main']['temp'];
+       var fiveDayWindValue = data[0]['wind']['speed'];
+       var fiveDayHumidityValue = data[0]['main']['humidity'];
+       
+       cityName.innerHTML = 'City: '+ cityNameValue; 
+          forecastTemp.innerHTML = 'Temperature: '+ fiveDayTempValue;
+          forecastWind.innerHTML = 'Wind: '+ fiveDayWindValue;
+          forecastHumidity.innerHTML = 'Humidity: '+ fiveDayHumidityValue;
+
+          console.log(data);
 
 }
 
 button.addEventListener('click', getCityLocation);
 
-      /* button.addEventListener('click', function(){
-        var userInputValue = userInput.value;
-        var geoCodeApiUrl = 'https://api.openweathermap.org/geo/1.0/direct?q='+userInputValue+'&limit=5&appid=e17d6c40bd10dd5e7fb0d651afd5e253'
-        
-            fetch(geoCodeApiUrl)
-            .then(response => response.json())
-            .then(data => {
-                 var name = data[1]['name']; 
-                var temp = data['main']['temp'];
-                var descValue = data['weather'][0]['description'];
 
-                 place.innerHTML = placeValue; 
-                temp.innerHTML = tempValue;
-                desc.innerHTML = descValue;
-
-            })
-             .catch(error => alert("Wrong!"))      
-        })   */
