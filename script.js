3bda137265ba768933f8cec9f2f89bffec990c07
efogerson1 <efogerson2@gmail.com>
2023-06-12 14:49:32 -0700

    updating edits

--- script.js
+++ script.js
@@ -8,12 +8,11 @@ var fiveDayTemp = document.querySelector('.forecastTemp')
 var fiveDayHumidity = document.querySelector('.forecastHumidity')
 var fiveDayWind = document.querySelector('.forecastWind')
 
-
-/*  Test function with console.log */
-  var apiUrlTest = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=e17d6c40bd10dd5e7fb0d651afd5e253'
+var apiUrlTest = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=e17d6c40bd10dd5e7fb0d651afd5e253'
 
 var geoCodeApiUrl = 'https://api.openweathermap.org/geo/1.0/direct?q='+userInput.value+'&limit=5&appid=e17d6c40bd10dd5e7fb0d651afd5e253' 
 
+/*  Test function with console.log */
 
 function getCityLocation(){
   var userInputValue = userInput.value;
@@ -33,58 +32,50 @@ function getCityLocation(){
 }
 
 function singleDay(lat, lon){
-  var currentWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=e17d6c40bd10dd5e7fb0d651afd5e253`
+  var currentWeather = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=e17d6c40bd10dd5e7fb0d651afd5e253`
 
          fetch(currentWeather)
          .then(response => response.json())
          .then(data => {
-         
-           var cityNameValue = data['name'];
-           var tempValue = data['main']['temp'];
-           var windValue = data['wind']['speed'];
-           var humidityValue = data['main']['humidity'];
+          console.log(data.list[0].main.temp);
+           var cityNameValue = data.city.name;
+           var tempValue = data.list[0].main.temp;
+           var windValue = data.list[0].wind.speed;
+           var humidityValue = data.list[0].main.humidity;
 
           cityName.innerHTML = 'City: '+ cityNameValue; 
           temp.innerHTML = 'Temperature: '+ tempValue;
           wind.innerHTML = 'Wind: '+ windValue;
           humidity.innerHTML = 'Humidity: '+ humidityValue;
 
-            
-          console.log(data);
-
          })
-         /* .catch(error => alert(error)); */
 
 }
 
 function fiveDayForecast(lat, lon){
-  var fiveForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=e17d6c40bd10dd5e7fb0d651afd5e253`
+ var fiveForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=e17d6c40bd10dd5e7fb0d651afd5e253`
 
-  /* var fiveDayTempValue = {};
+  var fiveDayTempValue = {};
   var fiveDayWindValue = {};
-  var fiveDayHumidityValue = {}; */ 
+  var fiveDayHumidityValue = {};  
 
   fetch(fiveForecast)
          .then(response => response.json())
          .then(data => {
-            /* fiveDayTempValue = data['main']['temp'];
-            fiveDayWindValue = data[0]['wind']['speed'];
-           fiveDayHumidityValue = data[0]['main']['humidity'];  */
           for (let i = 0; i < data.list.length; i += 8){
-            console.log(data.list[i]);
-
+             console.log(data.list[i]); 
+          cityNameValue = data.city.name;*/
+          fiveDayTempValue = `${data.list[i].main.temp}`;
+          fiveDayWindValue = data.list[i].wind.speed;
+          fiveDayHumidityValue = data.list[i].main.humidity;
           }
          }); 
-
-       
-       
-       cityName.innerHTML = 'City: '+ cityNameValue; 
+          
+          cityName.innerHTML = 'City: '+ cityNameValue;
           forecastTemp.innerHTML = 'Temperature: '+ fiveDayTempValue;
           forecastWind.innerHTML = 'Wind: '+ fiveDayWindValue;
-          forecastHumidity.innerHTML = 'Humidity: '+ fiveDayHumidityValue;
-
-          console.log(data);
-
+          forecastHumidity.innerHTML = 'Humidity: '+ fiveDayHumidityValue; 
+          
 }
 
 button.addEventListener('click', getCityLocation);
